251050dc6e43542ebcabe4163fbc17b74aa0c93e
ademcan <ademcaniyi7@gmail.com>
2025-05-29 23:44:06 +0300

    sunucu izin kontrolü

---
+++ check_permissions.php
@@ -0,0 +1,34 @@
+<?php
+// Kontrol edilecek klasör yolu
+$directory = '/home/acd1f4ftwarecom/acdisoftware.online/gym_bro/vendor';
+
+// Fonksiyon izinleri kontrol eder
+function checkAndSetPermissions($directory)
+{
+    if (!is_dir($directory)) {
+        echo "Dizin bulunamadı: $directory";
+        return;
+    }
+
+    // Geçerli izinleri al
+    $permissions = substr(sprintf('%o', fileperms($directory)), -3);
+
+    echo "Mevcut izinler: $permissions\n";
+
+    if ($permissions !== '755') {
+        echo "İzinler doğru değil. Düzeltme yapılıyor...\n";
+
+        // İzinleri düzelt
+        if (chmod($directory, 0755)) {
+            echo "İzinler başarıyla 755 olarak ayarlandı.\n";
+        } else {
+            echo "İzinler ayarlanırken hata oluştu.\n";
+        }
+    } else {
+        echo "İzinler zaten doğru.\n";
+    }
+}
+
+// Fonksiyonu çalıştır
+checkAndSetPermissions($directory);
+?>
