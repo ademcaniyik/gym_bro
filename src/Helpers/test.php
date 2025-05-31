3a7b5a3653c3006435d935b40c074ccf345630e3
ademcan <ademcaniyi7@gmail.com>
2025-06-01 00:52:00 +0300

    kütüphane işleri

--- src/Helpers/test.php
+++ src/Helpers/test.php
@@ -1,17 +1,13 @@
 <?php
 
-// 1. Autoload dosyasını yükle
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
-// 2. Dotenv sınıfını kullan
 use Dotenv\Dotenv;
 
 try {
-    // 3. .env dosyasını yükle
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/../../'); // .env dosyası bir üst dizinde
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
     $dotenv->load();
 
-    // 4. .env dosyasından bir değişkeni oku ve ekrana yaz
     echo "DB_HOST: " . $_ENV['DB_HOST'] . "<br>";
     echo "DB_NAME: " . $_ENV['DB_NAME'] . "<br>";
     echo "DB_USER: " . $_ENV['DB_USER'] . "<br>";
@@ -19,6 +15,5 @@ try {
 
     echo "<br>Dotenv çalışıyor! 🎉";
 } catch (Exception $e) {
-    // Hata durumunda hata mesajını yazdır
     echo "Dotenv yüklenirken bir hata oluştu: " . $e->getMessage();
 }
